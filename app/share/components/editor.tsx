d4a7918c470360a82a9dcc599dd657a19e4f3de0
Pranjal Maurya <pranjalmaurya01@gmail.com>
2024-12-18 11:22:29 +0530

    fix: useSearchParam Suspense

--- app/share/components/editor.tsx
+++ app/share/components/editor.tsx
@@ -4,7 +4,7 @@
 import { cn } from '@/lib/utils';
 import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 import CodeMirror, { ReactCodeMirrorRef } from '@uiw/react-codemirror';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { Suspense, useCallback, useEffect, useRef, useState } from 'react';
 import { EditorContext } from './EditorContext';
 import { EditorSidebar } from './editorSideMenu';
 import { EditorTabs, TabState } from './editorTabs';
@@ -47,62 +47,64 @@ function Editor({ id }: { id: string | null }) {
   }, [isEditorReady]);
 
   return (
-    <EditorContext.Provider value={{ id, theme: selectedTheme }}>
-      <div className='flex'>
-        <EditorSidebar />
-        <div className='flex-1'>
-          <div className='flex flex-col h-screen'>
-            <EditorTabs
-              onTabClose={(id: string) => {
-                setTabs((prev) => {
-                  const openTabs = prev.all.filter((t) => t.id !== id);
-                  return {
-                    ...prev,
-                    all: openTabs,
-                    active: openTabs.length ? openTabs[0].id : '',
-                  };
-                });
-              }}
-              onTabChange={(id: string) => {
-                setTabs((prev) => ({ ...prev, active: id }));
-              }}
-              tabs={tabs.all}
-              activeTab={tabs.active}
-            />
-            {tabs.active ? (
-              <>
-                {!isEditorReady && <div className='flex-1 bg-black/90' />}
-                <CodeMirror
-                  ref={editorRef}
-                  basicSetup={{
-                    foldGutter: true,
-                  }}
-                  className={cn('flex-1', isEditorReady ? 'block' : 'hidden')}
-                  height='100%'
-                  value={value}
-                  onChange={onChange}
-                  theme={vscodeDark}
-                  onCreateEditor={() => {
-                    setIsEditorReady(true);
+    <Suspense fallback={<div className='h-screen w-screen bg-black/90' />}>
+      <EditorContext.Provider value={{ id, theme: selectedTheme }}>
+        <div className='flex'>
+          <EditorSidebar />
+          <div className='flex-1'>
+            <div className='flex flex-col h-screen'>
+              <EditorTabs
+                onTabClose={(id: string) => {
+                  setTabs((prev) => {
+                    const openTabs = prev.all.filter((t) => t.id !== id);
+                    return {
+                      ...prev,
+                      all: openTabs,
+                      active: openTabs.length ? openTabs[0].id : '',
+                    };
+                  });
+                }}
+                onTabChange={(id: string) => {
+                  setTabs((prev) => ({ ...prev, active: id }));
+                }}
+                tabs={tabs.all}
+                activeTab={tabs.active}
+              />
+              {tabs.active ? (
+                <>
+                  {!isEditorReady && <div className='flex-1 bg-black/90' />}
+                  <CodeMirror
+                    ref={editorRef}
+                    basicSetup={{
+                      foldGutter: true,
+                    }}
+                    className={cn('flex-1', isEditorReady ? 'block' : 'hidden')}
+                    height='100%'
+                    value={value}
+                    onChange={onChange}
+                    theme={vscodeDark}
+                    onCreateEditor={() => {
+                      setIsEditorReady(true);
+                    }}
+                  />
+                </>
+              ) : (
+                <div
+                  className={cn(
+                    'border-b border-zinc-800 h-full',
+                    !selectedTheme && 'bg-black/90 text-white'
+                  )}
+                  style={{
+                    backgroundColor: selectedTheme?.backgroundColor,
+                    color: selectedTheme?.color,
                   }}
                 />
-              </>
-            ) : (
-              <div
-                className={cn(
-                  'border-b border-zinc-800 h-full',
-                  !selectedTheme && 'bg-black/90 text-white'
-                )}
-                style={{
-                  backgroundColor: selectedTheme?.backgroundColor,
-                  color: selectedTheme?.color,
-                }}
-              />
-            )}
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </EditorContext.Provider>
+      </EditorContext.Provider>
+    </Suspense>
   );
 }
 export default Editor;
--- app/share/page.tsx
+++ app/share/page.tsx
@@ -6,11 +6,15 @@ import Editor from './components/editor';
 // https://docs.convex.dev/client/react/nextjs/server-rendering
 
 export default function Page() {
-  const searchParams = useSearchParams();
-
   return (
-    <Suspense fallback={<div className='h-screen w-screen bg-black/90' />}>
-      <Editor id={searchParams.get('id')} />
+    <Suspense>
+      <EditorWrapper />
     </Suspense>
   );
+}
+
+function EditorWrapper() {
+  const searchParams = useSearchParams();
+
+  return <Editor id={searchParams.get('id')} />;
 }
